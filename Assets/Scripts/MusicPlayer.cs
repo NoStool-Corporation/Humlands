a4c236c18f7f5f97f13395f6fe8529697ed2b7cf
Shaddaaa <florian@kaufmann.cloud>
2019-03-20 20:40:18 +0100

    added the ability to play music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource audioSource;
+    int clipAmount = 1;
+    AudioClip[] clips;
+    int currentClip;
+    int delay = 30000;
+    long nextPlay;
+
+    String musicDir = "Music/";
+
+    void Start()
+    {
+        clips = new AudioClip[clipAmount];
+        for (int i = 0; i < clipAmount; i++)
+        {
+            print(musicDir + i + ".mp3");
+            clips[i] = Resources.Load<AudioClip>(musicDir + i);
+        }
+
+        nextPlay = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        currentClip = 0;
+    }
+
+    void Update()
+    {
+        print(nextPlay);
+        print(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+        if (clipAmount >= 0 && nextPlay <= DateTimeOffset.UtcNow.ToUnixTimeMilliseconds())
+        {
+            print("t");
+
+            audioSource.clip = clips[currentClip];
+            audioSource.Play();
+
+            nextPlay = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() + (int)(audioSource.clip.length * 1000) + delay;
+
+            currentClip++;
+            if(currentClip <= clipAmount)
+            {
+                currentClip = 0;
+            }
+        }
+    }
+}
