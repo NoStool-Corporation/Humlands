c4a634df8f9275c6d4eab94d0f9a24c5a3153f8e
Shaddaaa <florian@kaufmann.cloud>
2019-03-20 21:32:26 +0100

    added scripts to play music

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -19,25 +19,25 @@ public class CameraMovement : MonoBehaviour
     {
         Vector3 pos = transform.position;
 
-        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        if (Input.GetKey(KeyCode.W) /*|| Input.mousePosition.y >= Screen.height - panBorderThickness*/)
         {
             Vector3 direction = new Vector3(Mathf.Sin(transform.rotation.eulerAngles.y * degToRad), 0, Mathf.Cos(transform.rotation.eulerAngles.y * degToRad));
             pos += moveSpeed * Time.deltaTime * direction;
         }
-        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= panBorderThickness)
+        if (Input.GetKey(KeyCode.S) /*|| Input.mousePosition.y <= panBorderThickness*/)
         {
             Vector3 direction = new Vector3(Mathf.Sin(transform.rotation.eulerAngles.y * degToRad), 0, Mathf.Cos(transform.rotation.eulerAngles.y * degToRad));
             pos -= moveSpeed * Time.deltaTime * direction;
         }
-        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= panBorderThickness)
-        {
-            Vector3 direction = new Vector3(Mathf.Cos(transform.rotation.eulerAngles.y * degToRad), 0, Mathf.Sin(transform.rotation.eulerAngles.y * degToRad));
-            pos += moveSpeed * Time.deltaTime * direction;
-        }
-        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        if (Input.GetKey(KeyCode.A) /*|| Input.mousePosition.x <= panBorderThickness*/)
         {
             Vector3 direction = new Vector3(Mathf.Cos(transform.rotation.eulerAngles.y * degToRad), 0, Mathf.Sin(transform.rotation.eulerAngles.y * degToRad));
             pos -= moveSpeed * Time.deltaTime * direction;
+        }
+        if (Input.GetKey(KeyCode.D) /*|| Input.mousePosition.x >= Screen.width - panBorderThickness*/)
+        {
+            Vector3 direction = new Vector3(Mathf.Cos(transform.rotation.eulerAngles.y * degToRad), 0, Mathf.Sin(transform.rotation.eulerAngles.y * degToRad));
+            pos += moveSpeed * Time.deltaTime * direction;
         }
 
         transform.position = pos;
