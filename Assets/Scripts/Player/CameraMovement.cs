1f83d47a59f3498ce67506368c583eebbbb8ab9c
Shaddaaa <florian@kaufmann.cloud>
2019-03-26 21:16:06 +0100

    fixed a bug where you would lose control of camera rotation

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -15,6 +15,8 @@ public class CameraMovement : MonoBehaviour
 
     Vector3? pivot;
 
+    bool moveBorder = true;
+
     void Update()
     {
         Movement();
@@ -26,11 +28,28 @@ public class CameraMovement : MonoBehaviour
     /// </summary>
     void Movement()
     {
+        float fwMovement = Input.GetAxis("Vertical");
+        float swMovement = Input.GetAxis("Horizontal");
+
+        if (moveBorder)
+        {
+            if (Input.mousePosition.y >= Screen.height - moveBorderThickness)
+                fwMovement++;
+            if (Input.mousePosition.y <= moveBorderThickness)
+                fwMovement--;
+            if (Input.mousePosition.x <= moveBorderThickness)
+                swMovement--;
+            if (Input.mousePosition.x >= Screen.width - moveBorderThickness)
+                swMovement++;
+        }
+
         //basic WASD movement
         Vector3 forward = new Vector3(Round(Mathf.Sin(transform.rotation.eulerAngles.y * degToRad), 3), 0, Round(Mathf.Cos(transform.rotation.eulerAngles.y * degToRad), 3));
-        transform.position += forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        transform.position += forward * moveSpeed * Time.deltaTime * fwMovement;
         Vector3 left = new Vector3(Round(Mathf.Sin((transform.rotation.eulerAngles.y + 90) * degToRad), 3), 0, Round(Mathf.Cos((transform.rotation.eulerAngles.y + 90) * degToRad), 3));
-        transform.position += left * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        transform.position += left * moveSpeed * Time.deltaTime * swMovement;
+
+
 
         //zooming with the scroll wheel
         transform.position += transform.forward * Input.GetAxis("Mouse ScrollWheel") * 20;
@@ -60,11 +79,23 @@ public class CameraMovement : MonoBehaviour
                 pivot = TerrainControl.GetBlockPos(hit).ToVector3();
             }
             transform.RotateAround((Vector3)pivot, -new Vector3(Round(transform.right.x, 3), 0, Round(transform.right.z, 3)), Input.GetAxis("Mouse Y"));
+            if (transform.rotation.eulerAngles.x < 15)
+                transform.rotation = Quaternion.Euler(15, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
         }
         else
         {
             pivot = null;
         }
+    }
+
+    private static float UnwrapAngle(float angle)
+    {
+        if (angle >= 0)
+            return angle;
+
+        angle = -angle % 360;
+
+        return 360 - angle;
     }
 
     float Round(float f, int precision)
