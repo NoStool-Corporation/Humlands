a67ab065277e2351f747dafcf51e065f02255a65
leoleander <leo-gruetzner@t-online.de>
2019-04-25 18:19:31 +0200

    Added the ability to generate dynamic 45° rotated render squares around the camera according to the "renderDistance" attribute.

--- Assets/Scripts/World/WorldGeneration/LoadChunks.cs
+++ Assets/Scripts/World/WorldGeneration/LoadChunks.cs
@@ -10,7 +10,7 @@ public class LoadChunks : MonoBehaviour
     /// <summary>
     /// TEMPORARY ridiculous array of chunks to load around the player, send help
     /// </summary>
-    static WorldPos[] chunkPositions= {    new WorldPos( 0, 0,  0), new WorldPos(-1, 0,  0), new WorldPos( 0, 0, -1), new WorldPos( 0, 0,  1), new WorldPos( 1, 0,  0),
+    /*static WorldPos[] chunkPositions = {    new WorldPos( 0, 0,  0), new WorldPos(-1, 0,  0), new WorldPos( 0, 0, -1), new WorldPos( 0, 0,  1), new WorldPos( 1, 0,  0),
                                             new WorldPos(-1, 0, -1), new WorldPos(-1, 0,  1), new WorldPos( 1, 0, -1), new WorldPos( 1, 0,  1), new WorldPos(-2, 0,  0),
                                             new WorldPos( 0, 0, -2), new WorldPos( 0, 0,  2), new WorldPos( 2, 0,  0), new WorldPos(-2, 0, -1), new WorldPos(-2, 0,  1),
                                             new WorldPos(-1, 0, -2), new WorldPos(-1, 0,  2), new WorldPos( 1, 0, -2), new WorldPos( 1, 0,  2), new WorldPos( 2, 0, -1),
@@ -48,11 +48,13 @@ public class LoadChunks : MonoBehaviour
                                             new WorldPos( 7, 0, -2), new WorldPos( 7, 0,  2), new WorldPos(-7, 0, -3), new WorldPos(-7, 0,  3), new WorldPos(-3, 0, -7),
                                             new WorldPos(-3, 0,  7), new WorldPos( 3, 0, -7), new WorldPos( 3, 0,  7), new WorldPos( 7, 0, -3), new WorldPos( 7, 0,  3),
                                             new WorldPos(-6, 0, -5), new WorldPos(-6, 0,  5), new WorldPos(-5, 0, -6), new WorldPos(-5, 0,  6), new WorldPos( 5, 0, -6),
-                                            new WorldPos( 5, 0,  6), new WorldPos( 6, 0, -5), new WorldPos( 6, 0,  5) };
+                                            new WorldPos( 5, 0,  6), new WorldPos( 6, 0, -5), new WorldPos( 6, 0,  5) };*/
 
+    private WorldPos[] chunkPositions;
+    
     int renderHeight = 2;
 
-    public int renderDistance;
+    public int renderDistance = 8;
 
     /// <summary>
     /// Timer to only unload chunks every 10 update ticks
@@ -62,6 +64,7 @@ public class LoadChunks : MonoBehaviour
     private void Start()
     {
         world = GameObject.Find("World").GetComponent<World>();
+        chunkPositions = GetRenderDistanceChunks();
     }
 
     void Update()
@@ -82,6 +85,8 @@ public class LoadChunks : MonoBehaviour
             Mathf.FloorToInt(transform.position.y / Chunk.chunkSize) * Chunk.chunkSize,
             Mathf.FloorToInt(transform.position.z / Chunk.chunkSize) * Chunk.chunkSize
             );
+
+        WorldPos[] chunkPositions = GetRenderDistanceChunks();
 
         if (renderList.Count == 0)
         {
@@ -125,7 +130,7 @@ public class LoadChunks : MonoBehaviour
             for (int i = 0; i < buildList.Count && i < 8; i++)
             {
                 Chunk chunk = world.BuildChunk(buildList[0]);
-                if(chunk != null)
+                if (chunk != null)
                     chunk.renderNeighbors = true;
 
                 buildList.RemoveAt(0);
@@ -157,7 +162,7 @@ public class LoadChunks : MonoBehaviour
                 float distance = Vector3.Distance(
                     new Vector3(chunk.Value.pos.x, 0, chunk.Value.pos.z),
                     new Vector3(transform.position.x, 0, transform.position.z));
-                if (distance > Chunk.chunkSize*10 && !chunk.Value.stayLoaded)
+                if (distance > Chunk.chunkSize * 10 && !chunk.Value.stayLoaded)
                     chunksToDelete.Add(chunk.Key);
             }
             foreach (var chunk in chunksToDelete)
@@ -179,24 +184,46 @@ public class LoadChunks : MonoBehaviour
         return false;
     }
 
-    public List<Vector3> GetRenderDistanceChunks() {
+    /// <summary>
+    /// Generates an array of all chunks inside a 45° rotated square arround the camera
+    /// </summary>
+    /// <returns></returns>
+    public WorldPos[] GetRenderDistanceChunks() {
         if (renderDistance < 1) {
+            Debug.Log(renderDistance);
             return null;
         }
+        
+        WorldPos[] ret = new WorldPos[(renderDistance - 1) * renderDistance * 2 + 1];
+        int i = 0;
 
-        int size = renderDistance * 2 + 1;
-        List<Vector3> ret = new List<Vector3>(size*size*size);
+        ret[i] = new WorldPos(0, 0, 0);
+        i++;
 
-        for (int z = -renderDistance; z < size; z++) {
-            for (int y = -renderDistance; y < size; y++)
-            {
-                for (int x = -renderDistance; x < size; x++)
-                {
-                    ret.Add(new Vector3(x, y, z));
-                }
+        for (int z = 1; z < renderDistance; z++)
+        {
+            for (int x = 0; x < z; x++) {
+                ret[i] = new WorldPos(x, 0, z - x);
+                i++;
+                ret[i] = new WorldPos(ret[i - 1].z, 0, ret[i - 1].x * - 1);
+                i++;
+                ret[i] = new WorldPos(ret[i - 2].x * - 1, 0, ret[i - 2].z * -1);
+                i++;
+                ret[i] = new WorldPos(ret[i - 3].z * - 1, 0, ret[i - 3].x);
+                i++;
             }
         }
-
+        Debug.Log(i);
+        Debug.Log(ret.Length);
         return ret;
+    }
+
+    /// <summary>
+    /// Updates the render distance and creates a new "nearby-chunks-square"
+    /// </summary>
+    /// <param name="r"></param>
+    public void UpdateRenderDistance(int r) {
+        renderDistance = r;
+        chunkPositions = GetRenderDistanceChunks();
     }
 }
