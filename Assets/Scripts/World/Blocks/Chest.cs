cead0c2ee950b01162562cb38a1ca091717feeaf
RationalRick <r.shiri@gmx.de>
2019-05-05 22:35:25 +0200

    Added Chest

---
+++ Assets/Scripts/World/Blocks/Chest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public Inventory inventory;
+
+
+    public Chest() : base()
+    {
+        inventory = new Inventory(3000);
+    }
+}
